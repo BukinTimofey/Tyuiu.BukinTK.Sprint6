d18f7dac7297e94c8e58f3fafc706fc4ccf8a7b2
forengeit345 <timophey2907@gmail.com>
2025-12-05 16:14:46 +0500

    Task3_1

--- Tyuiu.BukinTK.Sprint6.Task3.V29.Lib/DataService.cs
+++ Tyuiu.BukinTK.Sprint6.Task3.V29.Lib/DataService.cs
@@ -6,27 +6,22 @@ namespace Tyuiu.BukinTK.Sprint6.Task3.V29.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
-
-            if (cols < 4)
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
             {
-                throw new ArgumentException("Матрица должна иметь 5 столбцов");
-            }
-            int[,] resultMatrix = (int[,])matrix.Clone();
-            for (int i = 0; i < rows - 1; i++)
-            {
-                for (int j = 0; j < rows - i - 1; j++)
+                for (int j = 0; j < matrix.GetLength(0) - i - 1; j++)
                 {
-                    if (resultMatrix[j, 3] > resultMatrix[j + 1, 3])
+                    if (matrix[j, 4] > matrix[j + 1, 4])
                     {
-                        int temp = resultMatrix[j, 3];
-                        resultMatrix[j, 3] = resultMatrix[j + 1, 3];
-                        resultMatrix[j + 1, 3] = temp;
+                        for (int k = 0; k < matrix.GetLength(1); k++)
+                        {
+                            int temp = matrix[j, k];
+                            matrix[j, k] = matrix[j + 1, k];
+                            matrix[j + 1, k] = temp;
+                        }
                     }
                 }
             }
-            return resultMatrix;
+            return matrix;
         }
     }
 }
