11e301e64c9729e57b9688e3cbfd93ae92cdaa45
forengeit345 <timophey2907@gmail.com>
2025-12-07 11:50:46 +0500

    Task6_1

--- Tyuiu.BukinTK.Sprint6.Task6.V16/FormMain.cs
+++ Tyuiu.BukinTK.Sprint6.Task6.V16/FormMain.cs
@@ -1,3 +1,5 @@
+using Tyuiu.BukinTK.Sprint6.Task6.V16.Lib;
+
 namespace Tyuiu.BukinTK.Sprint6.Task6.V16
 {
     public partial class FormMain : Form
@@ -6,5 +8,8 @@ namespace Tyuiu.BukinTK.Sprint6.Task6.V16
         {
             InitializeComponent();
         }
+
+        DataService ds = new DataService();
+        string filePath = "";
     }
 }
